89975e2ff57d08d99ea3727d40b3b111e4c4787d
dantxal <dantxal@gmail.com>
2020-10-30 16:36:31 -0300

    Markdown support + Navigation to posts

---
+++ content/coding-blog.md
@@ -0,0 +1,20 @@
+---
+slug: coding-blog
+title: Code and Images
+author: Imran Irshad
+description: Coding Post For Beautiful Code
+date: 30-September-2020
+---
+
+# React Functional Component
+
+```jsx
+import React from "react";
+
+const CoolComponent = () => <div>I'm a cool component!!</div>;
+
+export default CoolComponent;
+```
+## Images
+![Alt Text](test.jpg)
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
-import { GetStaticProps } from "next"
-import Head from 'next/head'
+import { GetStaticProps } from "next";
+import Head from 'next/head';
+import Link from 'next/link';
+import matter from 'gray-matter';
 
-export default function Home({title, description}) {
+export default function Home({title, description, data}) {
+  const realData = data.map((blog) => matter(blog));
+  const listItems = realData.map((item) => item.data)
+
   return (
     <>
       <Head>
@@ -11,17 +16,42 @@ export default function Home({title, description}) {
           <title>{ title }</title>
       </Head>
       <div>Hello world</div>
+
+      <ul>
+        {listItems.map(( blog, i ) => (
+          <li key={i}>
+            <Link href={`blog/${blog.slug}`} >
+              {blog.title}
+            </Link>
+            <p>{blog.description}</p>
+          </li>
+        ))}
+      </ul>
     </>
     )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const siteData = await import(`../../config.json`)
+  const fs = require('fs');
+
+  const files = fs.readdirSync(`${process.cwd()}/content`, "utf-8");
+  const blogs = files.filter((filename) => filename.endsWith(".md"))
+
+  const data = blogs.map((postFile) => {
+    const path = `${process.cwd()}/content/${postFile}`
+    const rawContent = fs.readFileSync(path, {
+      encoding: "utf-8",
+    })
+
+    return rawContent
+  });
 
   return {
     props: {
       title: siteData.default.title,
       description: siteData.default.description,
+      data,
     }
   }
 }
